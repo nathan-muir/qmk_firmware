6be91b5a8b7d208faeeffa025d0adf85f4c25a4c
Nathan Muir <ndmuir@gmail.com>
2018-09-11 10:54:30 +1000

    my keymap

---
+++ keyboards/ergodox_ez/keymaps/colemak-dh-21/config.h
@@ -0,0 +1,16 @@
+#include "../../config.h"
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+//#undef TAPPING_TOGGLE
+#undef TAPPING_TERM
+//#undef IGNORE_MOD_TAP_INTERRUPT
+//#undef PERMISSIVE_HOLD
+//
+//#define TAPPING_TOGGLE  1
+#define TAPPING_TERM    250
+//#define IGNORE_MOD_TAP_INTERRUPT
+////#define PERMISSIVE_HOLD
